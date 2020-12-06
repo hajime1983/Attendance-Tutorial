7218767e2080d90954df45379227ff3659f0b439
EC2 Default User <runbird@outlook.jp>
2020-12-06 19:33:28 +0000

    Finish login-function

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # ログイン機能
   get     '/login', to: 'sessions#new'
   post    '/login', to: 'sessions#create'
-  delete  '/logiin', to: 'sessions#destroy'
+  delete  '/logout', to: 'sessions#destroy'
   
   resources :users
 end
