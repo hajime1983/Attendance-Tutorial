7c1f68159c4e2963fe243c5b9f545724c184c97d
EC2 Default User <runbird@outlook.jp>
2020-12-20 20:58:22 +0000

    10.6.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   $days_of_the_week = %w{日 月 火 水 木 金 土}
-  
+end  
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month
-    @first_day = params[:date].nil? ?
+    @first_day = params[:date].nil? ? 
     Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。
@@ -14,9 +14,9 @@ class ApplicationController < ActionController::Base
     @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     
     unless one_month.count == @attendances.count # それぞれの件数（日数）が一致するか評価します。
-    　ActiveRecord::Base.transaction do # トランザクションを開始します。
-    　# 繰り返し処理により、1ヶ月分の勤怠データを生成します。
-    　one_month.each { |day| @user.attendances.create!(worked_on: day) }
+      ActiveRecord::Base.transaction do # トランザクションを開始します。
+      # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
+      one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
       @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
@@ -25,4 +25,3 @@ class ApplicationController < ActionController::Base
     flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
     redirect_to root_url
   end
-end
