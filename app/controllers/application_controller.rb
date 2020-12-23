edc68dadec62d5f443b88a9b2b1950aaeda8b18d
EC2 Default User <runbird@outlook.jp>
2020-12-23 20:39:52 +0000

    11.3.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,8 +41,8 @@ class ApplicationController < ActionController::Base
     
     unless one_month.count == @attendances.count # それぞれの件数（日数）が一致するか評価します。
       ActiveRecord::Base.transaction do # トランザクションを開始します。
-      # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
-      one_month.each { |day| @user.attendances.create!(worked_on: day) }
+        # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
+        one_month.each { |day| @user.attendances.create!(worked_on: day) }
       end
       @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
